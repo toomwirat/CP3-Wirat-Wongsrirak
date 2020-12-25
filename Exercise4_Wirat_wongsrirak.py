b842dbf49ede4e45a55d780b8982d2ee50d15261
toomwirat <66100105+toomwirat@users.noreply.github.com>
2020-12-25 10:01:06 +0700

    Create Exercise4_Wirat_wongsrirak.py

---
+++ Exercise4_Wirat_wongsrirak.py
@@ -0,0 +1,10 @@
+FE = 75.5
+GB = 78.0
+ICP = 80.0
+CP = 82.5
+
+print("--- Your Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICP)
+print("Computer Programming :", CP)
